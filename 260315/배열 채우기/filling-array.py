c4d2172b6b93fa69abec0a87a969db62e1056bc9
hmstellaj <stellahmjoo@gmail.com>
2026-03-15 23:48:24 +0900

    Title: 배열 채우기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260315/배열 채우기/filling-array.py
+++ 260315/배열 채우기/filling-array.py
@@ -2,8 +2,9 @@ list_nums = list(map(int, input().split()))
 
 reverse_list = list_nums[::-1]
 
-if reverse_list[0] == 0:
-    reverse_list = reverse_list[1:]
+if 0 in reverse_list:
+    index_zero = reverse_list.index(0) 
+    reverse_list = reverse_list[index_zero + 1:]
 
 for n in reverse_list:
     print(n, end=' ')
